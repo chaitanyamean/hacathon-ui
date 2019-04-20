d7b8d1a3bccd6a8aa4f6fc3298aa52e0dba80ccd
ioskyc <Hack2Hire23@gmail.com>
2019-04-20 18:13:25 +0530

    login signup pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {CustomMaterialModule} from './custom-material/custom-material.module';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './signup-login/sign-up/sign-up.component';
+import { LoginComponent } from './signup-login/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent, 
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatCheckboxModule
+    CustomMaterialModule,
+    RouterModule.forRoot([
+      {path: '', redirectTo: '/login', pathMatch:'full'},
+      {path: 'signup', component: SignUpComponent},
+      {path: 'login', component: LoginComponent}
+    ])
   ],
-  exports: [MatButtonModule, MatCheckboxModule],
   providers: [],
   bootstrap: [AppComponent]
 })
