362c430f4a448c1b0aae6bd6a4a3c2a505b199af
Venkatesh Chetty <venkateshchetty@Venkateshs-MacBook-Air.local>
2019-04-20 22:44:19 +0530

    upfdatev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { AdminModuleModule } from './admin-module/admin-module.module';
+import { UserModuleModule } from './user-module/user-module.module';
+import { HrModule } from './hr/hr/hr.module';
+import { HrRoutingModule } from './hr/hr/hr-routing.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -11,10 +17,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatCheckboxModule
-  ],
+    MatCheckboxModule,
+    AdminModuleModule,
+    UserModuleModule,
+    HrModule,
+    HrRoutingModule],
   exports: [MatButtonModule, MatCheckboxModule],
   providers: [],
   bootstrap: [AppComponent]
